b2a54cd344b363468877506bf03847e7fe07a56f
AEWebPro <hmmodaa_boy@yahoo.com>
2016-12-10 13:13:24 +0200

    App part 3 - The layout structure for login

--- app/src/main/java/com/example/ae/yora/activities/MainActivity.java
+++ app/src/main/java/com/example/ae/yora/activities/MainActivity.java
@@ -1,11 +1,12 @@
 package com.example.ae.yora.activities;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends BaseAuthenticatedActivity{
 
     @Override
     protected void onYoraCreate(Bundle savedState) {
-
+        Log.i("Main Activity", "We are here");
     }
 }
